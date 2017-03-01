a1006a7b48bcfc48c6203e30984ba59d779bc77a
Tim Coote <tim@coote.org>
2017-03-01 12:00:27 +0000

    python3 and python2 compatible. also documented some dependencies

--- c.py
+++ c.py
@@ -9,18 +9,13 @@ class ScanDelegate(DefaultDelegate):
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
-            print "Discovered device", dev.addr
+            print ("Discovered device", dev.addr)
         elif isNewData:
-            print "Received new data from", dev.addr
+            print ("Received new data from", dev.addr)
 
-class HRM(Peripheral):
-    def __init__(self, addr):
-        print ("making an HRM at {}".format (addr))
-        Peripheral.__init__(self, addr, addrType=ADDR_TYPE_RANDOM)
 
 if __name__=="__main__":
 
-    hrm = None
     scanner = Scanner ().withDelegate(ScanDelegate())
 
     devs = scanner.scan (5.0)
@@ -28,22 +23,32 @@ if __name__=="__main__":
     mac = ''
 
     for dev in devs:
-        print ("Devices {}, ({}) RSSI {}".format (dev.addr, dev.addrType, dev.rssi))
+#        print ("Devices {}, ({}) RSSI {}".format (dev.addr, dev.addrType, dev.rssi))
         mac = dev.addr
-        print (dev.__class__, mac)
+#        print (dev.__class__, mac)
 
     try:
         conn = Peripheral (mac, ADDR_TYPE_RANDOM)
+        conn.getServices ()  # needed to get the services property populated?
     except BTLEException:
         pass
 
-    print ("peripheral {}, services {} [{}]".format (conn, dir (conn), conn.getServices ()))
+#    print ("peripheral {}, services {}".format (conn, conn.services))
 
     try:
-        for svc in conn.services:
-            print (str (svc), svc.__class__, ":")
+        for svcuuid in conn.services:
+            svc = conn.getServiceByUUID (svcuuid)
+            print (str (svc), ":")
             for ch in svc.getCharacteristics ():
                 print ("    {}, hnd={}, supports {}".format (ch, hex (ch.handle), ch.propertiesToString ()))
-                chName = AssignedNumbers.getCommonName (ch.uuid)
+#                chName = AssignedNumbers.getCommonName (ch.uuid)
+#                print ("characteristic name {}".format (chName))
+
+                if (ch.supportsRead()):
+                    try:
+                        print("    ->", repr(ch.read()))
+                    except BTLEException as e:
+                        print("Error: ->", e)
+
     finally:
         conn.disconnect ()
